a34376b57356cad2ffdac8f6c3e0a5b86dc4a77d
Andrew Osheroff <andrewosh@gmail.com>
2016-05-15 21:43:39 -0400

    first working commit

--- index.js
+++ index.js
@@ -5,26 +5,48 @@ var mkdir = require('mkdirp')
 var nugget = require('nugget')
 var homePath = require('home-path')
 var mv = require('mv')
-var debug = require('debug')('java-download')
+var debug = require('debug')('prebuilt-download')
 var npmrc = require('rc')('npm')
 var URL = require('./url.js')
 var _ = require('lodash')
 
-var versions = require('./java_versions.json')
-
+/**
+ * opts can contain the following required/optional fields:
+ *  - name: name of the prebuilt (i.e. electron, java)
+ *  - url: template string optionally containing version, arch, and platform fields
+ *  - filename: template string optionally containing version, arch, and platform fields
+ *  - version: version to download
+ *  - arch: (optional) function that transforms os.arch into the correct form
+ *  - platform: (optional) function that transforms os.platform into the correct form
+ *  - requestOpts: (optional) additional HTTP request options
+ */
 module.exports = function download (opts, cb) {
   if (!opts.version) return cb(new Error('must specify version'))
+  if (!opts.url) return cb(new Error('must specify a URL template string'))
+  if (!opts.filename) return cb(new Error('must specify a filename template string'))
+  if (!opts.name) return cb(new Error('must specify a name'))
 
-  opts = getLatestMatch(opts)
-  if (!opts) return cb(new Error('no matching Java version'))
-  var url = URL(opts)
-  var filename = urlFilename(opts)
+  var buildString = function (template, arch, platform, version) {
+    var str = template.replace(/\{arch\}/g, arch)
+    str = str.replace(/\{platform\}/g, platform) 
+    str = str.replace(/\{version\}/g, version)
+    return str
+  }
+
+  var arch = (opts.arch) ? opts.arch(os.arch()) : os.arch()
+  var platform = (opts.platform) ? opts.platform(os.platform()) : os.platform()
+  var version = opts.version
+  console.log('arch: ', arch)
+  
+  // construct the url and filenames based on the above options
+  
+  var url = buildString(opts.url, arch, platform, version)
+  var filename = buildString(opts.filename, arch, platform, version)
 
   var homeDir = homePath()
-  var cache = opts.cache || path.join(homeDir, './.java')
+  var cache = opts.cache || path.join(homeDir, './.node_prebuilts/' + opts.name)
 
-  opts = URL(url)
-  debug('platform/arch', opts.platform, opts.arch)
+  debug('platform/arch', platform, arch)
 
   var strictSSL = true
   if (opts.strictSSL === false) {
@@ -53,24 +75,24 @@ module.exports = function download (opts, cb) {
     if (err) return cb(err)
     cachedZip = path.join(actualCache, filename) // in case cache dir changed
     // download to tmpdir
-    var tmpdir = path.join(os.tmpdir(), 'java-tmp-download-' + process.pid + '-' + Date.now())
+    var tmpdir = path.join(os.tmpdir(), opts.name + '-tmp-download-' + process.pid + '-' + Date.now())
     mkdir(tmpdir, function (err) {
       if (err) return cb(err)
       debug('downloading zip', url, 'to', tmpdir)
-      var nuggetOpts = {
+      var defaultOpts = {
         target: filename,
         dir: tmpdir,
         resume: true,
         verbose: true,
         strictSSL: strictSSL,
         proxy: proxy,
-        headers: { 'Cookie': 'oraclelicense=accept-securebackup-cookie' }
       }
+      var nuggetOpts = _.merge(defaultOpts, opts.requestOpts) 
       nugget(url, nuggetOpts, function (errors) {
         if (errors) {
           var error = errors[0] // nugget returns an array of errors but we only need 1st because we only have 1 url
           if (error.message.indexOf('404') === -1) return cb(error)
-          error.message = 'Failed to find Java v' + opts.version + ' for ' + opts.platform + '-' + opts.arch + ' at ' + url
+          error.message = 'Failed to find ' + opts.name + ' v' + opts.version + ' for ' + platform + '-' + arch + ' at ' + url
           return cb(error)
         }
         // when dl is done then put in cache
@@ -88,7 +110,7 @@ module.exports = function download (opts, cb) {
       if (err) {
         if (err.code !== 'EACCES') return cb(err)
         // try local folder if homedir is off limits (e.g. some linuxes return '/' as homedir)
-        var localCache = path.resolve('./.java')
+        var localCache = path.resolve('./node_prebuilts/' + opts.name)
         debug('local cache', localCache)
         return mkdir(localCache, function (err) {
           if (err) return cb(err)
@@ -98,16 +120,4 @@ module.exports = function download (opts, cb) {
       cb(null, cache)
     })
   }
-
-  function urlFilename (url) {
-    return [
-      [url.type, url.rev, url.platform, url.arch].join('-'),
-      url.ext
-    ].join('.')
-  }
-
-  function getLatestMatch (opts) {
-    return _.find(_.reverse(_.sortBy(versions, ['version', 'rev'])), opts)
-  }
-
 }
--- test.js
+++ test.js
@@ -1,35 +1,61 @@
-var isEqual = require('lodash.isequal')
+var fs = require('fs')
+var os = require('os')
+var path = require('path')
 
-function testurl () {
-  var url = require('./url.js')
+var test = require('tape')
+var homePath = require('home-path')
+var _ = require('lodash')
 
-  var good_str = 'http://download.oracle.com/otn-pub/java/jdk/8u92-b13/jre-8u92-linux-x64.tar.gz'
-  var good_obj = {
-    type: 'jre',
-    build: 'b13',
-    file: 'jre-8u92-linux-x64.tar.gz',
-    rev: 92,
-    version: 8,
-    platform: 'linux',
-    arch: 'x64',
-    ext: 'tar.gz'
+var download = require('./index')
+
+test('electron download test', function (t) {
+  t.plan(2)
+  var filename = 'electron-v{version}-{platform}-{arch}.zip'
+  download({
+    name: 'electron',
+    filename: filename,
+    version: '1.1.0',
+    url: 'https://github.com/electron/electron/releases/download/v{version}/' + filename
+  }, function (err, p) {
+    t.error(err)
+    var desiredName = 'electron-v1.1.0-' + os.platform() + '-' + os.arch() +'.zip'
+    var desiredPath = path.join(homePath(), './.node_prebuilts/electron/' + desiredName)
+    fs.unlinkSync(desiredPath)
+    t.equal(p, desiredPath)
+  })
+})
+
+test('docker download test', function (t) {
+  t.plan(2)
+  var filename = 'docker-{version}.tgz'
+  if (os.platform() !== 'linux') {
+    console.log('performing test using the linux prebuilt')
+    var forcedPlatform = true
   }
-
-  if (!isEqual(good_obj, url(good_str))) {
-    console.log('FAIL url.dismantle')
-    console.log(JSON.stringify(good_obj, null, 2))
-    console.log(JSON.stringify(url(good_str), null, 2))
-    process.exit(1)
-  }
-
-  if (!(good_str, url(good_obj))) {
-    console.log('FAIL url.construct')
-    console.log(JSON.stringify(good_str, null, 2))
-    console.log(JSON.stringify(url(good_obj), null, 2))
-    process.exit(1)
-  }
-
-  console.log('OK urls')
-}
-
-testurl()
+  download({
+    name: 'docker',
+    filename: filename,
+    version: '1.11.0',
+    arch: function (a) {
+      if (a === 'ia32') {
+        return 'i386'
+      } else if (a === 'x64') {
+        return 'x86_64'
+      }
+      return a
+    },
+    platform: function (p) {
+      if (forcedPlatform) {
+        return 'Linux'
+      }
+      return _.capitalize(p)
+    },
+    url: 'https://get.docker.com/builds/{platform}/{arch}/' + filename
+  }, function (err, p) {
+    t.error(err)
+    var desiredName = 'docker-1.11.0.tgz'
+    var desiredPath = path.join(homePath(), './.node_prebuilts/docker/' + desiredName) 
+    fs.unlinkSync(desiredPath)
+    t.equal(p, desiredPath)
+  })
+})
